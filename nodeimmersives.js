12b8057b1f3f8b83bdefa7ca05e02fc10b642e5d
Timothy Do <tydo83@gmail.com>
2020-09-28 15:15:38 -0400

    added comments and changed message

--- nodeimmersives.js
+++ nodeimmersives.js
@@ -1,9 +1,15 @@
+// Simple node app using process.argv[] and console.log();
+// process.argv[] takes an input from users as an argument 
+// console.log prints out statement in the parenthesis on the terminal 
+
+// User have to type first name and last name as arguments after nodeimmersives.js
+
 let firstName = process.argv[2];
 let lastName = process.argv[3];
 let firstInitial = firstName[0];
 let secondInitial = lastName[0];
 
-console.log("Hello " + firstName + " " + lastName);
-console.log("Welcome to Code Immersives, " + firstName.toUpperCase() + " " + lastName.toUpperCase());
-console.log("We are going to learn how to make an app using node!!! " + firstInitial + "." + secondInitial + ".");
-console.log("This is your email address: " + firstName.toLowerCase() + lastName.toLowerCase() + "@codeimmersives.com")
+console.log("Welcome to Code Immersives. " + firstName + " " + lastName);
+console.log("This line capitalized your name: " + firstName.toUpperCase() + " " + lastName.toUpperCase());
+console.log("This line prints out your initial: " + firstInitial + "." + secondInitial + ".");
+console.log("This line gives you Code immersives: " + firstName.toLowerCase() + lastName.toLowerCase() + "@codeimmersives.com")
